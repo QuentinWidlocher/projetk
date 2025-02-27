536a7fc5c6e0a9e0e89eef6fd929c6f98d622166
Quentin Widlocher <quentin@widlocher.com>
2025-02-27 16:37:52 +0100

    debug lvev

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 class_name Player
-extends Node2D
+extends CharacterBody2D
 
-@onready var rb: CharacterBody2D = %CharacterBody2D
+@export var speed: float = 200
 
 func _ready():
 	pass
@@ -12,5 +12,6 @@ func _process(_delta):
 func _physics_process(_delta):
 	var x_axis = Input.get_axis("move_left", "move_right")
 	var y_axis = Input.get_axis("move_up", "move_down")
-	# rb.velocity = Vector2(x_axis, y_axis) * 100
-	# rb.move_and_slide()
+	velocity = Vector2(x_axis, y_axis) * speed
+	print(velocity)
+	move_and_slide()
